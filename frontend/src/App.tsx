82e615f675084f049bc74c276112e5b66dd3e207
Paulo <paulow2011@gmail.com>
2022-01-29 11:06:32 -0300

    Primeiro Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
