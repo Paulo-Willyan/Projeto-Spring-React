93875a2be7789a20c0c637918db88684493a9019
Paulo <paulow2011@gmail.com>
2022-01-29 12:13:45 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import DataTable from "components/DataTable";
 import NavBar from 'components/NavBar';
 import Footer from 'components/Footer';
+
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <NavBar />
       <div className="container">
         <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />
       </div>
       <Footer />
     </>
